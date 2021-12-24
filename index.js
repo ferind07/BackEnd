1a6825989530443820e080e8526e09304b73ecca
ferind07 <96059330+ferind07@users.noreply.github.com>
2021-12-24 16:43:24 +0700

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const app = express();
 //app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended:true }));
+app.use(cors());
 
 app.get("/coba", (req, res) => {
   res.send("hello world");
