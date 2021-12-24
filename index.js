a3b290335aef45eb8401071dc1f8f73cd6d531d6
ferind07 <96059330+ferind07@users.noreply.github.com>
2021-12-24 10:27:55 +0700

    rapikan indent

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ const io = require('socket.io')(server);
 const users = {};
 
 io.on('connection', socket => {
-    if (!users[socket.id]) {
-        users[socket.id] = socket.id;
-    }
-    socket.emit("yourID", socket.id);
-    io.sockets.emit("allUsers", users);
-    socket.on('disconnect', () => {
-      delete users[socket.id];
-      console.log(users);
-      io.emit("allUsers", users);
-    })
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    console.log(users);
+    io.emit("allUsers", users);
+  })
 
-    socket.on("callUser", (data) => {
-      io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-    })
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
+  })
 
-    socket.on("acceptCall", (data) => {
-      io.to(data.to).emit('callAccepted', data.signal);
-    })
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  })
 });
 
 server.listen(8000, () => {
