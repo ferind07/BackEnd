18bb114592ea8c5995316839713da02061d3e435
ferind07 <96059330+ferind07@users.noreply.github.com>
2021-12-24 09:55:37 +0700

    update

--- index.js
+++ index.js
@@ -1,57 +1,32 @@
 const express = require("express");
-const http = require("http");
 const app = express();
 
-const chat = require("./router/chat");
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const cors = require("cors");
-app.use(cors);
-
-const server = http.createServer(app);
-const socket = require("socket.io");
-const req = require("express/lib/request");
-const io = socket(server);
-
-const users = [];
+const users = {};
 
 io.on('connection', socket => {
-  
-  if (!users[socket.id]) {
-    users.push(socket.id)
-  }
-  console.log(users);
-
-  socket.emit("yourID", socket.id);
-
-  io.sockets.emit("allUsers", users);
-
-  socket.on('disconnect', () => {
-    const id = socket.id;
-    for (let index = 0; index < users.length; index++) {
-      const element = users[index];
-      if (element === id){
-        users.splice(index, 1);
-      }
+    if (!users[socket.id]) {
+        users[socket.id] = socket.id;
     }
-    io.emit("allUsers", users);
-    console.log(users);
-  })
+    socket.emit("yourID", socket.id);
+    io.sockets.emit("allUsers", users);
+    socket.on('disconnect', () => {
+      delete users[socket.id];
+      console.log(users);
+      io.emit("allUsers", users);
+    })
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-  })
+    socket.on("callUser", (data) => {
+      io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
+    })
 
-  socket.on("acceptCall", (data) => {
-    io.to(data.to).emit('callAccepted', data.signal);
-  })
-});
-
-app.use("/chat", chat);
-
-app.get("/", (req, res) => {
-  res.send("Hello world")
+    socket.on("acceptCall", (data) => {
+      io.to(data.to).emit('callAccepted', data.signal);
+    })
 });
 
 server.listen(8000, () => {
-  console.log("Server running");
+  console.log("server run...");
 });
