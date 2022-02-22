6470a9ef0e05ae2b475a48abc1aa841cc9428371
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-02-22 23:25:37 +0700

    update

--- skripsi.sql
+++ skripsi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Feb 17, 2022 at 04:10 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.1.2
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2022 at 05:24 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -22,6 +22,19 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `skripsi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `skripsi`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructor`
+--
+
+CREATE TABLE `instructor` (
+  `idUser` int(11) NOT NULL,
+  `katagori` int(11) NOT NULL,
+  `valid` int(11) NOT NULL,
+  `berkas` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -46,11 +59,21 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `name`, `phoneNumber`, `role`, `saldo`, `status`, `image`) VALUES
-(29, 'ferryindra1122@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Ferry', '089514322227', 2, 0, 1, '');
+(29, 'ferryindra1122@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Ferry', '089514322227', 2, 0, 0, ''),
+(31, 'ferryindra25@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'ferry', '089514322227', 1, 0, 1, ''),
+(32, 'stef@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'steffany', '089514322227', 1, 0, 1, ''),
+(33, 'agus@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'agus', '081234150225', 2, 0, 1, ''),
+(34, 'admin@admin.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', '089514322227', 3, 0, 1, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD KEY `FK_idUser` (`idUser`);
 
 --
 -- Indexes for table `user`
@@ -66,7 +89,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD CONSTRAINT `FK_idUser` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
