b93b6714e6ff19d4cab86d280767a9944cdbfa29
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-03-07 21:22:37 +0700

    Update skripsi.sql

--- skripsi.sql
+++ skripsi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 25, 2022 at 08:55 PM
+-- Generation Time: Mar 07, 2022 at 03:17 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.2
 
@@ -36,8 +36,16 @@ CREATE TABLE `class` (
   `title` varchar(255) NOT NULL,
   `detail` text NOT NULL,
   `duration` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id`, `idInstructor`, `title`, `detail`, `duration`, `price`, `image`) VALUES
+(3, 33, 'html & css', '<p>html &amp; css course</p>', 100, 100000, '/public/uploads/classImage/classImage-1646001497490.jpeg');
 
 -- --------------------------------------------------------
 
@@ -48,6 +56,7 @@ CREATE TABLE `class` (
 DROP TABLE IF EXISTS `instructor`;
 CREATE TABLE `instructor` (
   `idUser` int(11) NOT NULL,
+  `instructorDetail` varchar(50) NOT NULL,
   `katagori` int(11) NOT NULL,
   `valid` int(11) NOT NULL,
   `berkas` varchar(50) NOT NULL
@@ -57,8 +66,8 @@ CREATE TABLE `instructor` (
 -- Dumping data for table `instructor`
 --
 
-INSERT INTO `instructor` (`idUser`, `katagori`, `valid`, `berkas`) VALUES
-(33, 1, 1, '/public/uploads/berkas/berkas-1645713171115.pdf');
+INSERT INTO `instructor` (`idUser`, `instructorDetail`, `katagori`, `valid`, `berkas`) VALUES
+(33, '', 1, 1, '/public/uploads/berkas/berkas-1645713171115.pdf');
 
 -- --------------------------------------------------------
 
@@ -86,9 +95,49 @@ DROP TABLE IF EXISTS `schedule`;
 CREATE TABLE `schedule` (
   `id` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `duration` int(11) NOT NULL
+  `idClass` int(11) NOT NULL,
+  `duration` int(11) NOT NULL,
+  `dateStart` datetime NOT NULL,
+  `dateEnd` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`id`, `idUser`, `idClass`, `duration`, `dateStart`, `dateEnd`) VALUES
+(1, 31, 3, 100, '2022-03-09 11:15:00', '2022-03-09 12:55:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submission`
+--
+
+DROP TABLE IF EXISTS `submission`;
+CREATE TABLE `submission` (
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `idInstructor` int(11) NOT NULL,
+  `idClass` int(11) NOT NULL,
+  `dateStart` datetime NOT NULL,
+  `dateEnd` datetime NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `submission`
+--
+
+INSERT INTO `submission` (`id`, `idUser`, `idInstructor`, `idClass`, `dateStart`, `dateEnd`, `status`) VALUES
+(1, 31, 33, 3, '2022-03-09 11:15:00', '2022-03-09 12:55:00', 0),
+(2, 32, 33, 3, '2022-03-06 13:00:00', '2022-03-06 14:40:00', 0),
+(3, 32, 33, 3, '2022-03-07 13:00:00', '2022-03-07 14:40:00', 0),
+(4, 32, 33, 3, '2022-03-08 13:00:00', '2022-03-08 14:40:00', 0),
+(5, 31, 33, 3, '2022-03-06 12:30:00', '2022-03-06 14:10:00', 0),
+(6, 31, 33, 3, '2022-03-06 14:10:00', '2022-03-06 15:50:00', 0),
+(7, 31, 33, 3, '2022-03-06 14:10:00', '2022-03-06 15:50:00', 0),
+(8, 31, 33, 3, '2022-03-06 14:20:00', '2022-03-06 16:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -106,7 +155,7 @@ CREATE TABLE `user` (
   `role` smallint(1) NOT NULL,
   `saldo` int(11) NOT NULL DEFAULT 0,
   `status` tinyint(1) NOT NULL DEFAULT 1,
-  `image` varchar(50) NOT NULL DEFAULT ''
+  `image` varchar(100) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -114,8 +163,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `name`, `phoneNumber`, `role`, `saldo`, `status`, `image`) VALUES
-(29, 'ferryindra1122@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Ferry', '089514322227', 2, 0, 0, ''),
-(31, 'ferryindra25@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'ferry', '089514322227', 1, 0, 1, ''),
+(29, 'ferryindra1122@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Ferry', '089514322227', 2, 0, 1, ''),
+(31, 'ferryindra25@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'ferry', '089514322227', 1, 0, 1, '/public/uploads/userProfile/userProfile-1646643340487.jpeg'),
 (32, 'stef@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'steffany', '089514322227', 1, 0, 1, ''),
 (33, 'agus@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'agus', '081234150225', 2, 0, 1, ''),
 (34, 'admin@admin.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', '089514322227', 3, 0, 1, '');
@@ -143,6 +192,18 @@ ALTER TABLE `review`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `submission`
+--
+ALTER TABLE `submission`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -156,13 +217,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `submission`
+--
+ALTER TABLE `submission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
