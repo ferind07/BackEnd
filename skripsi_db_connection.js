f3cc755aeabbb3c767059dd15208577212dd9cac
ferind07 <96059330+ferind07@users.noreply.github.com>
2022-06-22 12:51:28 +0700

    Update skripsi_db_connection.js

--- skripsi_db_connection.js
+++ skripsi_db_connection.js
@@ -1,13 +1,13 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "skripsi"
+  host: "103.145.226.115",
+  user: "ferryind_ferry",
+  password: "ferry123",
+  database: "ferryind_skripsi",
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
